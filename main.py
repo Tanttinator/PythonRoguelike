225bd648e705e21acfb00a73acef4f94267b3bc6
Antti Kangas <antti.kangas99@gmail.com>
2020-08-14 17:24:45 +0300

    Part 3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tcod
 from engine import Engine
 from input_handlers import EventHandler
 from entity import Entity
-from game_map import GameMap
+from procgen import generate_dungeon
 
 def main() -> None:
     screen_width = 80
@@ -18,16 +18,15 @@ def main() -> None:
     map_width = 80
     map_height = 45
 
-    colors = {
-        'dark_wall': tcod.Color(0, 0, 100),
-        'dark_ground': tcod.Color(50, 50, 150)
-    }
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
    
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', tcod.white)
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', tcod.yellow)
     entities = {player, npc}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(max_rooms=max_rooms, room_min_size=room_min_size, room_max_size=room_max_size, map_width=map_width, map_height=map_height, player=player)
 
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
