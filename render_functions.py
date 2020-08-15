faf68aed4c2b9361b69924b9869f3f06de15b4e6
Antti Kangas <antti.kangas99@gmail.com>
2020-08-15 19:43:41 +0300

    Part 7

--- render_functions.py
+++ render_functions.py
@@ -1,28 +1,33 @@
-import tcod as libtcod
+from __future__ import annotations
 
-def render_all(con, entities, game_map, screen_width, screen_height, colors):
+from typing import TYPE_CHECKING
 
-    for y in range(game_map.height):
-        for x in range(game_map.width):
-            wall = game_map.tiles[x][y].block_sight
+import color
 
-            if wall:
-                libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
-            else:
-                libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+if TYPE_CHECKING:
+    from tcod import Console
+    from engine import Engine
+    from game_map import GameMap
 
-    for entity in entities:
-        draw_entity(con, entity)
+def get_names_at_location(x: int, y: int, game_map: GameMap) -> str:
+    if not game_map.in_bounds(x, y) or not game_map.visible[x, y]:
+        return ""
 
-    libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+    names = ", ".join(entity.name for entity in game_map.entities if entity.x == x and entity.y == y)
 
-def clear_all(con, entities):
-    for entity in entities:
-        clear_entity(con, entity)
+    return names.capitalize()
 
-def draw_entity(con, entity):
-    libtcod.console_set_default_foreground(con, entity.color)
-    libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+def render_bar(console: Console, current_value: int, max_value: int, total_width: int) -> None:
+    bar_width = int(float(current_value) / max_value * total_width)
 
-def clear_entity(con, entity):
-    libtcod.console_put_char(con, entity.x, entity.y, ' ', libtcod.BKGND_NONE)
+    console.draw_rect(x=0, y=45, width=20, height=1, ch=1, bg=color.bar_empty)
+
+    if bar_width > 0:
+        console.draw_rect(x=0, y=45, width=bar_width, height=1, ch=1, bg=color.bar_filled)
+
+    console.print(x=1, y=45, string=f"HP: {current_value}/{max_value}", fg=color.bar_text)
+
+def render_names_at_mouse_location(console: Console, x: int, y: int, engine: Engine) -> None:
+    mouse_x, mouse_y = engine.mouse_location
+    names_at_mouse_location = get_names_at_location(x=mouse_x, y=mouse_y, game_map=engine.game_map)
+    console.print(x=x, y=y, string=names_at_mouse_location)
