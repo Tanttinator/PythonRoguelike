ca268b78311042e6e1f0cfb114c186515a8eed0c
Antti Kangas <antti.kangas99@gmail.com>
2020-10-09 01:25:17 +0300

    Part 13

--- entity.py
+++ entity.py
@@ -9,6 +9,8 @@ from render_order import RenderOrder
 if TYPE_CHECKING:
     from components.ai import BaseAI
     from components.consumable import Consumable
+    from components.equipment import Equipment
+    from components.equippable import Equippable
     from components.fighter import Fighter
     from components.inventory import Inventory
     from components.level import Level
@@ -81,7 +83,8 @@ class Actor(Entity):
         char: str = "?", 
         color: Tuple[int, int, int] = (255, 255, 255), 
         name: str = "<Unnamed>", 
-        ai_cls: Type[BaseAI], 
+        ai_cls: Type[BaseAI],
+        equipment: Equipment, 
         fighter: Fighter,
         inventory: Inventory,
         level: Level,
@@ -89,6 +92,9 @@ class Actor(Entity):
         super().__init__(x=x, y=y, char=char, color=color, name=name, blocks_movement=True, render_order=RenderOrder.ACTOR,)
 
         self.ai: Optional[BaseAI] = ai_cls(self)
+
+        self.equipment = equipment
+        self.equipment.parent = self
 
         self.fighter = fighter
         self.fighter.parent = self
@@ -112,9 +118,17 @@ class Item(Entity):
         char: str = "?",
         color: Tuple[int, int, int] = (255, 255, 255),
         name: str = "<Unnamed>",
-        consumable: Consumable,
+        consumable: Optional[Consumable] = None,
+        equippable: Optional[Equippable] = None,
     ):
         super().__init__(x=x, y=y, char=char, color=color, name=name, blocks_movement=False, render_order=RenderOrder.ITEM)
 
         self.consumable = consumable
-        self.consumable.parent = self
+        
+        if self.consumable:
+            self.consumable.parent = self
+
+        self.equippable = equippable
+
+        if self.equippable:
+            self.equippable.parent = self
