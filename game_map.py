d8f854bc113befd461822e9b6bf0c95bacf238b3
Antti Kangas <antti.kangas99@gmail.com>
2020-10-09 00:15:02 +0300

    Part 12

--- game_map.py
+++ game_map.py
@@ -72,7 +72,7 @@ class GameMap:
 
 class GameWorld:
 
-    def __init__(self, *, engine: Engine, map_width: int, map_height: int, max_rooms: int, room_min_size: int, room_max_size: int, max_monsters_per_room: int, max_items_per_room: int, current_floor: int = 0):
+    def __init__(self, *, engine: Engine, map_width: int, map_height: int, max_rooms: int, room_min_size: int, room_max_size: int, current_floor: int = 0):
         self.engine = engine
 
         self.map_width = map_width
@@ -82,9 +82,6 @@ class GameWorld:
 
         self.room_min_size = room_min_size
         self.room_max_size = room_max_size
-
-        self.max_monsters_per_room = max_monsters_per_room
-        self.max_items_per_room = max_items_per_room
 
         self.current_floor = current_floor
 
@@ -99,7 +96,5 @@ class GameWorld:
             room_max_size = self.room_max_size,
             map_width = self.map_width,
             map_height = self.map_height,
-            max_monsters_per_room = self.max_monsters_per_room,
-            max_items_per_room = self.max_items_per_room,
             engine = self.engine
             )
